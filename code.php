641404f477d37047261ab62c6d905bf8d23d6436
Akhil <akhilbalantrapu98@gmail.com>
2019-06-09 23:12:17 +0530

    Create code.php

---
+++ code.php
@@ -0,0 +1,26 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . '/wordpress/wp-load.php');
+$args = array(
+'cat' => 4, // source posts from a specific category
+'posts_per_page' => 2 // Specify how many posts you'd like to display
+);
+$latest_posts = new WP_Query( $args );
+if ( $latest_posts->have_posts() ) {
+while ( $latest_posts->have_posts() ) {
+$latest_posts->the_post(); ?>
+<li>
+<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+<?php if ( has_post_thumbnail() ) { ?>
+<span class="post_thumbnail"><?php the_post_thumbnail(); ?></span>
+<?php } ?>
+<span class="post_title"><?php the_title(); ?></span>
+</a>
+<span class="post_time">Posted on <?php the_time('l jS F, Y') ?></span>
+<?php the_excerpt(); ?>
+</li>
+<? }
+} else {
+echo '<p>There are no posts available</p>';
+}
+wp_reset_postdata();
+?>
